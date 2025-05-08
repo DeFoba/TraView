e32ecf1b63a9729bc15d5bd2afdcef6e3604609f
DeFoba <51561763+DeFoba@users.noreply.github.com>
2025-05-08 18:27:09 +0300

    add footer & add scripts

--- static/main.js
+++ static/main.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// Ивент на скролл
 const element = document.getElementById('header');
 
 function checkScroll() {
@@ -15,3 +16,25 @@ function checkScroll() {
 window.addEventListener('load', checkScroll);
 
 window.addEventListener('scroll', checkScroll);
+
+
+// Проверка на windows
+const userAgent = window.navigator.userAgent;
+
+const isWindows = userAgent.includes('Windows');
+
+if (!isWindows) {
+    // Получаем все элементы с классом .special-element
+    const elements = document.getElementsByClassName('download-button');
+    
+    // Добавляем класс .non-windows ко всем элементам
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].classList.add('non-windows');
+    }
+    
+    // Меняем текст в элементах с классом .message
+    const messages = document.getElementsByClassName('download-button');
+    for (let i = 0; i < messages.length; i++) {
+        messages[i].innerText = 'Only windows';
+    }
+}
